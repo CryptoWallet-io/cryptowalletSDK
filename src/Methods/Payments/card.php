57c4afcfbc77896675c7836f141437e4447d5a2d
James Kirkby <me@jameskirkby.com>
2016-03-06 23:55:10 +0000

    update cardGateway file

---
+++ src/Methods/Payments/card.php
@@ -0,0 +1,34 @@
+<?php
+
+    /**
+     * Created by PhpStorm.
+     * User: Satoshi
+     * Date: 04/03/2016
+     * Time: 17:46
+     */
+
+    namespace CryptoWallet\Mehods\Payments;
+
+    use CryptoWallet\Client;
+
+    class CardGateway extends Client
+    {
+
+        public function __Construct($cryptowallet)
+        {
+            if(is_object($cryptowallet)){
+                $this->client = $cryptowallet->client;
+            } else {
+                throw new \Exception('You need to initialise the cryptowallet client to use this gateway');
+            }
+        }
+
+
+        /**
+         * @param array $paymentPayload
+         */
+        function createCardPayment(array $paymentPayload)
+        {
+
+        }
+    }
